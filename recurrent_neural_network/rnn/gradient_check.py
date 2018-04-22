8197a7c794991141f18f22b3ccadbf869015fd7f
Calvin Feng <cfeng@fetchrobotics.com>
2018-04-21 18:03:03 -0700

    Added temporal softmax loss

--- recurrent_neural_network/rnn/gradient_check.py
+++ recurrent_neural_network/rnn/gradient_check.py
@@ -2,7 +2,18 @@ import numpy as np
 
 
 def eval_numerical_gradient_array(f, x, df, h=1e-5):
-    """Evaluate a numeric gradient for a function that accepts a numpy array and returns a numpy array."""
+    """Evaluate a numeric gradient for a function that accepts a numpy array and returns a numpy array.
+    
+    Args:
+        f (lambda): Lambda function that accepts x as an input
+        x (np.array): Input of any shape, as long as f accepts it
+        df (np.array): Upstream gradient
+        h (float): Smaller number for taking limits
+
+    Returns:
+        grad (float): Gradient value    
+    """
+
     grad = np.zeros_like(x)
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
@@ -17,4 +28,39 @@ def eval_numerical_gradient_array(f, x, df, h=1e-5):
 
         grad[ix] = np.sum((pos - neg) * df) / (2 * h)
         it.iternext()
+    return grad
+
+
+def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
+    """Naive implementation of numerical gradient of f at x
+
+    Args:
+        f (lambda): Function that takes a single argument
+        x (np.array): The point (numpy array) to evaluate the gradient at
+
+    Returns:
+        grad (float): Gradient value
+    """
+
+    fx = f(x) # evaluate function value at original point
+    grad = np.zeros_like(x)
+    # iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+
+        # evaluate function at x+h
+        ix = it.multi_index
+        oldval = x[ix]
+        x[ix] = oldval + h # increment by h
+        fxph = f(x) # evalute f(x + h)
+        x[ix] = oldval - h
+        fxmh = f(x) # evaluate f(x - h)
+        x[ix] = oldval # restore
+
+        # compute the partial derivative with centered formula
+        grad[ix] = (fxph - fxmh) / (2 * h) # the slope
+        if verbose:
+            print(ix, grad[ix])
+        it.iternext() # step to next dimension
+
     return grad
---
+++ recurrent_neural_network/rnn/loss.py
@@ -0,0 +1,87 @@
+import numpy as np
+from gradient_check import eval_numerical_gradient
+
+
+def rel_error(x, y):
+    """Returns relative error"""
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+    
+
+def temporal_softmax_loss(score, y, mask, verbose=False):
+    """
+    Args:
+        score (np.array): Input scores, of shape (N, T, V)
+        y (np.array): Ground truth indices, of shape (N, T) where each element is in range 
+                      0 <= y[n, t] < V
+        mask (np.array): Boolean array of shape (N, T) where mask[n, t] tells whehter or not the 
+                         score at x[n, t] should contribute to the loss.
+
+    Returns tuple:
+        - loss (float): Scalar giving loss
+        - grad_score (np.array): Gradient of loss with respect to scores
+    """
+    N, T, V = score.shape
+    
+    # Reshape the inputs to flatten them
+    score_flat = score.reshape(N*T, V)
+    y_flat = y.reshape(N*T)
+    mask_flat = mask.reshape(N*T)
+
+    # Compute probabilities
+    probs = np.exp(score_flat - np.max(score_flat, axis=1, keepdims=True))
+    probs /= np.sum(probs, axis=1, keepdims=True)
+
+    # Compute loss
+    ################################################################################################
+    # Mask is composed of boolean values, i.e. True/False. When you multiply a numpy array with a 
+    # mask (which is just another numpy array), the false(s) will act as zeros.
+    # Example:
+    # mask = np.array([True, True, False, False])
+    # nums = np.array([1, 1, 1, 1])
+    # mask * nums => array([1, 1, 0, 0])
+    ################################################################################################
+    loss = -1 * np.sum(mask_flat * np.log(probs[np.arange(N*T), y_flat])) / N
+
+    grad_score_flat = probs.copy()
+    grad_score_flat[np.arange(N*T), y_flat] -= 1
+    grad_score_flat /= N
+    grad_score_flat *= mask_flat[:, None] # Needed to do this for broadcasting
+
+    if verbose:
+        print 'gradient of loss w.r.t score flat dimension:', grad_score_flat.shape
+    
+    grad_score = grad_score_flat.reshape(N, T, V)
+
+    return loss, grad_score
+
+
+
+def check_loss(N, T, V, p):
+    x = 0.001 * np.random.randn(N, T, V)
+    y = np.random.randint(V, size=(N, T))
+    mask = np.random.rand(N, T) <= p
+    print(temporal_softmax_loss(x, y, mask)[0])
+
+
+def main():
+    N, T, V = 100, 1, 10
+    check_loss(100, 1, 10, 1.0)   # Should be about 2.3
+    check_loss(100, 10, 10, 1.0)  # Should be about 23
+    check_loss(5000, 10, 10, 0.1) # Should be about 2.3
+
+    # Gradient check for temporal softmax loss
+    N, T, V = 7, 8, 9
+
+    x = np.random.randn(N, T, V)
+    y = np.random.randint(V, size=(N, T))
+    mask = (np.random.rand(N, T) > 0.5)
+
+    loss, dx = temporal_softmax_loss(x, y, mask, verbose=False)
+
+    dx_num = eval_numerical_gradient(lambda x: temporal_softmax_loss(x, y, mask)[0], x, verbose=False)
+    
+    print rel_error(dx, dx_num)
+
+
+if __name__ == "__main__":
+    main()
