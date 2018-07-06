936e769e1a0d95aae15807a7793edff40eee83c6
Calvin Feng <calvinjhfeng@gmail.com>
2018-07-05 21:42:07 -0700

    Completed style transfer

--- style_transfer/loss.py
+++ style_transfer/loss.py
@@ -1,28 +1,28 @@
 import tensorflow as tf
 
-
-def content_loss(content_weight, current, target):
+# The goal is to generate an image that reflects the content of one image and the style of another
+# by incorporating both in the loss function. 
+def content_loss(content_weight, current_features, target_features):
     """Compute the content loss for style transfer
 
-    The goal is to generate an image that reflects the content of one image and the style of another
-    by incorporating both in the loss function. Content loss measures how much the feature map of 
-    the generated image differs from the feature map of the source image. 
+    Content loss measures how much the feature map of the generated image differs from the feature 
+    map of the source image. 
 
     Args:
         content_weight (float): Scalar constant to multiply the content loss
-        current (Tensor): Features of the current image, with shape (1, H, W, C)
-        target (Tensor): Features of the content image, with shape (1, H, W, C)
+        current_features (Tensor): Features of the current_features image, with shape (1, H, W, C)
+        target_features (Tensor): Features of the content image, with shape (1, H, W, C)
     
     Returns:
         content_loss (float)
     """
-    H = tf.shape(current)[1]
-    W = tf.shape(current)[2]
-    C = tf.shape(current)[3]
+    H = tf.shape(current_features)[1]
+    W = tf.shape(current_features)[2]
+    C = tf.shape(current_features)[3]
 
-    current = tf.reshape(current, (H*W, C))
-    target = tf.reshape(target, (H*W, C))
-    loss = tf.reduce_sum((current - target) ** 2)
+    current_features = tf.reshape(current_features, (H*W, C))
+    target_features = tf.reshape(target_features, (H*W, C))
+    loss = tf.reduce_sum((current_features - target_features) ** 2)
 
     return content_weight * loss
 
@@ -50,5 +50,55 @@ def gram_matrix(features, normalize=True):
     return gram
 
 
-def style_loss():
-    pass
+def style_loss(features, style_layers, style_targets, style_weights):
+    """Compute style loss at a set of layers
+
+    Args:
+        features (Tensor): List of features at every layer of the current image, as produced by the extract_features
+                           function.
+        style_layers ([]int): List of layer indices into features giving the layers to include in the loss function.
+        style_targets ([]Tensor): List of the same length as style_layers, where style_targets[i] is a Tensor giving the
+                                  Gram matrix the source style image computed at layer style_layers[i].
+        style_weights ([]float): List of the same length as style_layers, where style_weights[i] is a scalar giving the
+                                 the weight for the style loss at layer style_layers[i].
+
+    Returns:
+        style_loss (Tensor): A tensor containing the scalar style loss.
+    """
+    style_loss = 0
+    
+    for i in range(len(style_layers)):
+        gram = gram_matrix(features[style_layers[i]])
+        gram_target = style_targets[i]
+        loss = (gram - gram_target)**2
+        style_loss += tf.reduce_sum(loss) * style_weights[i]
+    
+    return style_loss
+
+
+def total_variation_loss(img, tv_weight):
+    """Compute total variation loss
+
+    Adding an extra regularization term to the loss function will encourage smoothness in the final output image. 
+
+    Args:
+        img (Tensor): Tensor of shape (1, H, W, 3), holding an input image.
+        tv_weight (float): Scalar giving the weight to use for the total variation loss.
+
+    Returns:
+        loss (Tensor) Tensor holding a scalar giving the total variation loss for img weighted by tv_weight.
+    """
+    H = tf.shape(img)[1]
+    W = tf.shape(img)[2]
+
+    img = tf.reshape(img, (H, W, 3))
+    img_origin_vert_slice = tf.slice(img, [0, 0, 0], [H-1, W, 3])
+    img_vert_slice = tf.slice(img, [1, 0, 0], [H-1, W, 3])
+
+    img_origin_horz_slice = tf.slice(img, [0, 0, 0], [H, W-1, 3])
+    img_horz_slice = tf.slice(img, [0, 1, 0], [H, W-1, 3])
+
+    loss = tf.reduce_sum((img_origin_vert_slice - img_vert_slice)**2) + \
+        tf.reduce_sum((img_origin_horz_slice - img_horz_slice)**2)
+    
+    return tv_weight * loss
