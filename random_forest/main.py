169ab530758052c041db754c5bdbdf9980fe85c2
Calvin Feng <calvinjhfeng@gmail.com>
2018-07-29 17:30:10 -0700

    Add more notes regarding how to split a tree in jupyter notebook

--- random_forest/main.py
+++ random_forest/main.py
@@ -1,26 +1,14 @@
 from keras.utils import to_categorical
 from sklearn import tree
+from random_forest import build_tree, print_tree
 
 import numpy as np
 import csv
 
 
-def main():
-    x, y = [], []
-    col_names = None
-    with open('./datasets/credit_card_fraud.csv', 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        
-        # Extract column names from header
-        header = reader.next()
-        col_names = header[1:len(header) - 1]
-        
-        for row in reader:
-            x.append([float(el) for el in row[1:len(row)-1]])
-            y.append(int(row[-1]))
-
-    x = np.array(x).astype('float')
-    y = np.array(y).astype('int')
+def predict_with_sklearn_dtree(x_rows, y_labels):
+    x = np.array(x_rows).astype('float')
+    y = np.array(y_labels).astype('int')
 
     N = len(x)
     
@@ -33,7 +21,40 @@ def main():
     x_test = x[int(N*0.80):]
     y_test = to_categorical(y[int(N*0.80):], num_classes=2)
 
-    print model.score(x_test, y_test)
+    return model.score(x_test, y_test)
+
+
+def predict_with_dtree(col_names, x_rows, y_labels):
+    training_data = []
+
+    N = len(x_rows)
+    for i in range(int(N*0.80)):
+        training_data.append(x_rows[i] + [y_labels[i]])
+    
+    root = build_tree(col_names, training_data)
+    print_tree(root)
+
+    testing_data = []
+    for i in range(int(N*0.80), N):
+        testing_data.append(x_rows[i], [y_labels[i]])
+
+
+def main():
+    col_names = None
+    x, y = [], []
+
+    with open('./datasets/credit_card_fraud.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        
+        # Extract column names from header
+        header = reader.next()
+        col_names = header[1:len(header) - 1]
+        
+        for row in reader:
+            x.append([float(el) for el in row[1:len(row)-1]])
+            y.append(int(row[-1]))
+    
+    predict_with_dtree(col_names, x, y)
 
 
 if __name__ == '__main__':
--- random_forest/random_forest/tree.py
+++ random_forest/random_forest/tree.py
@@ -18,7 +18,7 @@ class Leaf(object):
         pred = self.class_counts.copy()
         for key in pred:
             pred[key] = float(pred[key])/norm
-        
+
         return pred
 
 
@@ -30,7 +30,8 @@ class DecisionNode(object):
 
 
 def find_best_split(col_names, rows):
-    """Find the best question to ask by iterating over every feature / value and calculating the information gain.
+    """Find the best question to ask by iterating over every feature / value and calculating the 
+    information gain.
     """
     best_gain = 0
     best_question = None
@@ -38,37 +39,39 @@ def find_best_split(col_names, rows):
     num_feats = len(rows[0]) - 1
 
     for col in range(num_feats):
-        values = set([row[col] for row in rows])  # Extract unique values from the dataset in a given column.
+        # Extract unique values from the dataset in a given column.
+        values = set([row[col] for row in rows])
         for val in values:
-            # Create a question, if the value is numeric, then the question is whether input is greater or equal to val.
-            # If the value is string, then the question si whether input is equal to val.
+            # Create a question, if the value is numeric, then the question is whether input is
+            # greater or equal to val. If the value is string, then the question si whether input is
+            # equal to val.
             question = Question(col_names[col], col, val)
 
             # Split the dataset
             true_rows, false_rows = partition(rows, question)
             if len(true_rows) == 0 or len(false_rows) == 0:
                 continue
-            
+
             # Calculate information gain from this split
             gain = info_gain(true_rows, false_rows, current_uncertainty)
 
             if gain >= best_gain:
                 best_gain, best_question = gain, question
-    
+
     return best_gain, best_question
 
 
 def build_tree(col_names, rows):
     """Build a decision tree recursively.
 
-    The function will return a top level decision node which has multiple branches if the rows are separable. One can 
-    use the root node to traverse through the tree and seek leaf nodes.
+    The function will return a top level decision node which has multiple branches if the rows are 
+    separable. One can use the root node to traverse through the tree and seek leaf nodes.
     """
     gain, question = find_best_split(col_names, rows)
 
     if gain == 0:
         return Leaf(rows)
-    
+
     true_rows, false_rows = partition(rows, question)
     true_branch = build_tree(col_names, true_rows)
     false_branch = build_tree(col_names, false_rows)
@@ -80,12 +83,12 @@ def print_tree(node, spacing=''):
     if isinstance(node, Leaf):
         print spacing + 'Predict', node.predictions
         return
-    
+
     print spacing + str(node.question)
 
     print spacing + '--> True:'
     print_tree(node.true_branch, spacing + '  ')
-    
+
     print spacing + '--> False:'
     print_tree(node.false_branch, spacing + '  ')
 
@@ -93,12 +96,12 @@ def print_tree(node, spacing=''):
 def classify(row, node):
     if isinstance(node, Leaf):
         return node.predictions
-    
+
     if node.question.match(row):
         return classify(row, node.true_branch)
-    
+
     return classify(row, node.false_branch)
-    
+
 
 if __name__ == '__main__':
     header = ['color', 'diameter', 'label']
@@ -112,7 +115,7 @@ if __name__ == '__main__':
     ]
 
     #####################
-    # Construct the tree 
+    # Construct the tree
     #####################
     root = build_tree(header, training_data)
     print_tree(root)
